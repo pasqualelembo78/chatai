680e9661511f5566b97d34fab6cbc058f487bd92
Your Name <you@example.com>
2026-07-13 21:32:29 +0000

    Fix: add missing Gravity import

--- app/src/main/java/com/intelligame/chatai/MessageAdapter.java
+++ app/src/main/java/com/intelligame/chatai/MessageAdapter.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.text.SpannableStringBuilder;
 import android.text.style.StyleSpan;
 import android.util.Base64;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
